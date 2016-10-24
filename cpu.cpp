14bafa779a0c17269c6a598524b8dc2f98fa7154
Evun Dunnell <edunnell@virtualhold.com>
2016-10-24 19:45:29 -0400

    iterating the rom_data using an iterator, commented out code not working as expected yet

--- cpu.cpp
+++ cpu.cpp
@@ -1,29 +1,52 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
 
-using std::cout; using std::endl; using std::string;
+using std::cout; using std::endl; using std::hex;
 using std::ifstream; using std::istreambuf_iterator; using std::noskipws;
-using std::vector; using std::for_each;
+using std::vector; using std::map; using std::for_each;
 
 // 16-bit registers
 unsigned short int pc;
 // 8-bit registers
 unsigned char sp, a, x, y, status;
 
-int main(int argc, char *argv[]) {
+// ??
+// enum operand_t { byte, word };
 
-  cout << "argv[1]: " << argv[1] << endl;
+// struct opcode_t{
+//   unsigned int opcode;
+//   operand_t    length;
+// };
+
+// typedef map<int, opcode_t> opcodes_t;
+// opcodes_t opcode_map = {
+//   {0xa9, 2} // lda immediate
+// };
+// unsigned char get_operand(operand_t::byte x) {
+// }
+// unsigned short int get_operand(operand_t::word x) {
+// }
+int main(int argc, char *argv[]) {
   ifstream rom(argv[1], std::ios::binary);
   noskipws(rom);
-  //  string rom_data{istreambuf_iterator<char>{rom}, {}};
   vector<unsigned char> rom_data{istreambuf_iterator<char>{rom}, {}};
-  cout << "rom_data: "<< endl;
-  //  cout << rom_data << endl;
-  for_each(rom_data.begin(), rom_data.end(), [](uint8_t &byte) { cout << std::hex << byte; });
-  cout << "rom_data[0]: " << rom_data[0] << endl;
+  auto rom_data_iter = rom_data.begin();
+  while(rom_data_iter != rom_data.end()) {
+    cout << std::hex << static_cast<int>(*rom_data_iter) << "|";
+    // opcodes_t::iterator opcode_iter;
+    // if((opcode_iter=opcodes_map.find(byte))!=opcodes_t::end()) {
+    //   for(int i=0; i < opcode_iter->second.length-1) {
+    //     //get_byte_operand()
+    //     //get_word_operand()
+    //     unsigned int operand = *(++rom_data_iter);
+    //     // emit_instruction(instruction, operatnd )
+    //   }
+    // }
+    ++rom_data_iter;
+  }
   
   return 0;
 }
