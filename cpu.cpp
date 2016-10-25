dad89ebb1898fb772e532eed907cebc64f5f368e
Evun Dunnell <edunnell@virtualhold.com>
2016-10-25 08:52:14 -0400

    hardcoded to print out all LDA instructions with their operand

--- cpu.cpp
+++ cpu.cpp
@@ -27,26 +27,15 @@ opcodes_t opcodes_map = {
   {0xa9, {"LDA", byte}} // lda immediate
 };
 
-// unsigned char get_operand(operand_t::byte);
-// unsigned short int get_operand(operand_t::word);
-
 int main(int argc, char *argv[]) {
   ifstream rom(argv[1], std::ios::binary);
   noskipws(rom);
   vector<unsigned char> rom_data{istreambuf_iterator<char>{rom}, {}};
   auto rom_data_iter = rom_data.begin();
   while(rom_data_iter != rom_data.end()) {
-    // cout << std::hex << static_cast<int>(*rom_data_iter) << "|";
     opcodes_t::iterator opcode_iter;
     if((opcode_iter=opcodes_map.find(*rom_data_iter))!=opcodes_map.end()) {
-      cout << opcode_iter->second.opcode << " " << opcode_iter->second.length << endl;
-      cout << std::hex << static_cast<int>(*rom_data_iter) << endl;
-      // for(int i=0; i < opcode_iter->second.length-1) {
-      //   //get_byte_operand()
-      //   //get_word_operand()
-      //   unsigned int operand = *(++rom_data_iter);
-      //   // emit_instruction(instruction, operatnd )
-      // }
+      cout << opcode_iter->second.opcode << " #" << std::hex << static_cast<int>(*(rom_data_iter+1))  << endl;
     }
     ++rom_data_iter;
   }
